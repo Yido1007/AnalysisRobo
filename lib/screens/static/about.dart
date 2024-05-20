59a18b630cb2368d59d5782a2daca90aac3979b6
example <email@example.com>
2024-05-20 21:19:06 +0300

    Created About Us page

--- lib/screens/static/about.dart
+++ lib/screens/static/about.dart
@@ -5,7 +5,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import '../../bloc/client/client_cubit.dart';
 
 class AboutScreen extends StatefulWidget {
-  const AboutScreen({super.key});
+  const AboutScreen({Key? key}) : super(key: key);
 
   @override
   State<AboutScreen> createState() => _AboutScreenState();
@@ -22,15 +22,35 @@ class _AboutScreenState extends State<AboutScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return BlocBuilder<ClientCubit, ClientState>(builder: (context, state) {
-      return Scaffold(
-        appBar: AppBar(
-          title: Text(
-            AppLocalizations.of(context).getTranslate("about"),
+    return BlocBuilder<ClientCubit, ClientState>(
+      builder: (context, state) {
+        return Scaffold(
+          appBar: AppBar(
+            title: Text(
+              AppLocalizations.of(context).getTranslate("about"),
+            ),
+            centerTitle: true,
           ),
-          centerTitle: true,
-        ),
-      );
-    });
+          body: SafeArea(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text(
+                    AppLocalizations.of(context).getTranslate("about_text"),
+                    style: const TextStyle(
+                      fontSize: 22,
+                      color: Colors.black87,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
+    );
   }
 }
